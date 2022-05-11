538f3f54175f83ccf829cd36f9f9c9c7d535370d
rmcfdma <104601350+rmcfdma@users.noreply.github.com>
2022-05-10 22:08:59 -0300

    Update plotar_ganho_kalman.m

--- plotar_ganho_kalman.m
+++ plotar_ganho_kalman.m
@@ -1,7 +1,18 @@
-function [outputArg1,outputArg2] = plotar_ganho_kalman(inputArg1,inputArg2)
-%PLOTAR_GANHO_KALMAN Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function plotar_ganho_kalman(t,L_k,w)
+%%     Função que calcula a norma 2 das duas colunas do Ganho de Kalman e mostra no gráfico.
+L_1 = norm(L_k(:,1),2);
+L_2 = norm(L_k(:,2),2);
+for i = 3:2:length(L_k)
+ L_1 = [L_1 norm(L_k(:,i),2)];
+ L_2 = [L_2 norm(L_k(:,i+1),2)];
 end
-
+plot(t,L_1','LineWidth',w)
+hold on
+plot(t,L_2','LineWidth',w)
+hold off
+title('Norma 2 das colunas do Ganho de Kalman vs.Tempo');
+ylabel('Ganho de Kalman');
+xlabel('Tempo - s');
+grid on;
+legend('Referente à Saída 1 - Posição do Carrinho','Referente à Saída 2 - Ângular da haste')
+end
