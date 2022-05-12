f9009c20d6c8570d519b1d5ff8d3e774a2cdeae2
rmcfdma <104601350+rmcfdma@users.noreply.github.com>
2022-05-12 11:03:26 -0300

    Update animar_pendulo.m

--- animar_pendulo.m
+++ animar_pendulo.m
@@ -5,22 +5,22 @@ function animar_pendulo(y1,y2,s,l_haste,l_carrinho,h_carrinho,titulo1,titulo2)
     % l_carrinho -> Comprimento do carrinho
     % h_carrinho -> Altura do carrinho
 
-    lh = l_haste*s;    % Comprimento da haste em escala
-    lc = l_carrinho*s; % Comprimento do carrinho em escala
-    hc = h_carrinho*s; % Altura do carrinho em escala
+    lh = l_haste*s;            % Comprimento da haste em escala
+    lc = l_carrinho*s;         % Comprimento do carrinho em escala
+    hc = h_carrinho*s;         % Altura do carrinho em escala
 if isempty(y2)
     for i = 1:length(y1)
-        clf();    % Limpa a figura anterior
-        hold on;  % Retém o gráfico corrente
-        axis([-4,4,-3,3]); % Define os eixos x e y  
-        grid on;  % Habilita a grade
+        clf();                 % Limpa a figura anterior
+        hold on;               % Retém o gráfico corrente
+        axis([-4,4,-3,3]);     % Define os eixos x e y  
+        grid on;               % Habilita a grade
         plot([ y1(i,1), y1(i,1)+lh*sin(y1(i,2))], [0, lh*cos(y1(i,2))],'blue','LineWidth',2);  % Desenha a haste apartir da posição do carrinho e ângulo da haste corrente  
         plot(y1(i,1)+[-lc/2,lc/2,lc/2,-lc/2,-lc/2], [0,0,-hc,-hc,0],'magenta','LineWidth',2); % Desesnha o carrinho a partir da posição corrente 
         title(strcat('Animação -',char(160),titulo1));
         xlabel('Posição do carrinho - m');
         ylabel(strcat('l*Cos ',char(160),char(952)));
-        hold off;    % Libera o gráfico corrente
-        drawnow(); % Atualiza a figura com os dados anteriores
+        hold off;              % Libera o gráfico corrente
+        drawnow();             % Atualiza a figura com os dados anteriores
     end
 else
     tiledlayout(1,2)
@@ -33,7 +33,7 @@ else
     xlabel('Posição do carrinho - m');
     ylabel(strcat('l*Cos ',char(160),char(952)));
     
-    if length(y1) >= length(y2)
+    if length(y1) >= length(y2) % No caso de 2 vetores de tamanhos diferentes especifica o maior e o menor
         maior = y1
         menor = y2
     else
@@ -42,23 +42,25 @@ else
     end     
     
         for i = 1:length(maior)
-            cla(ax1);    % Limpa a figura anterior
-            hold(ax1,'on');  % Retém o gráfico corrente
-            axis(ax1,[-4,4,-3,3]); % Define os eixos x e y
-            grid(ax1,'on');  % Habilita a grade
-            p1 = plot(ax1,[maior(i,1), maior(i,1)+lh*sin(maior(i,2))], [0, lh*cos(maior(i,2))],'blue','LineWidth',2);  % Desenha a haste apartir da posição do carrinho e ângulo da haste corrente  
-            p2 = plot(ax1,maior(i,1)+[-lc/2,lc/2,lc/2,-lc/2,-lc/2], [0,0,-hc,-hc,0],'magenta','LineWidth',2); % Desesnha o carrinho a partir da posição corrente 
-            hold(ax1,'off');  % Retém o gráfico corrente            
-            if i <= length(menor)
-                cla(ax2);    % Limpa a figura anterior
-                hold(ax2,'on');  % Retém o gráfico corrente
-                axis(ax2,[-4,4,-3,3]); % Define os eixos x e y             
-                grid(ax2,'on');  % Habilita a grade
+            if isgraphics(ax1)
+                cla(ax1);                             % Limpa a figura anterior
+                hold(ax1,'on');                       % Retém o gráfico corrente
+                axis(ax1,[-4,4,-3,3]);                % Define os eixos x e y
+                grid(ax1,'on');                       % Habilita a grade
+                p1 = plot(ax1,[maior(i,1), maior(i,1)+lh*sin(maior(i,2))], [0, lh*cos(maior(i,2))],'blue','LineWidth',2);  % Desenha a haste apartir da posição do carrinho e ângulo da haste corrente  
+                p2 = plot(ax1,maior(i,1)+[-lc/2,lc/2,lc/2,-lc/2,-lc/2], [0,0,-hc,-hc,0],'magenta','LineWidth',2); % Desesnha o carrinho a partir da posição corrente 
+                hold(ax1,'off');                      % Retém o gráfico corrente
+            end
+            if i <= length(menor) & isgraphics(ax2)   % Continua animando o maior e para o menor e verifica se ax2 ainda é válido
+                cla(ax2);                             % Limpa a figura anterior
+                hold(ax2,'on');                       % Retém o gráfico corrente
+                axis(ax2,[-4,4,-3,3]);                % Define os eixos x e y             
+                grid(ax2,'on');                       % Habilita a grade
                 p3 = plot(ax2,[menor(i,1), menor(i,1)+lh*sin(menor(i,2))], [0, lh*cos(menor(i,2))],'blue','LineWidth',2);  % Desenha a haste apartir da posição do carrinho e ângulo da haste corrente  
                 p4 = plot(ax2,menor(i,1)+[-lc/2,lc/2,lc/2,-lc/2,-lc/2], [0,0,-hc,-hc,0],'magenta','LineWidth',2); % Desesnha o carrinho a partir da posição corrente        
-                hold(ax2,'off');    % Libera o gráfico corrente
+                hold(ax2,'off');                      % Libera o gráfico corrente
             end
-            drawnow(); % Atualiza a figura com os dados anteriores
+            drawnow();                                % Atualiza a figura com os dados anteriores
         end     
 end
 end
